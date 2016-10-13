45854dc14b1e21eb7816284a40e38e11e6c16b8a
tomeken-yoshinaga <kenta-yoshinaga@aist.go.jp>
2016-10-13 13:40:45 +0900

    [update] Fix bug.

--- encodeCheck.jl
+++ encodeCheck.jl
@@ -42,7 +42,7 @@ end
 doc = flattenDoc(doc)
 
 numOfData = length(doc)
-numOfTrainData = Int(floor(0.9 * numOfData)
+numOfTrainData = Int(floor(0.9 * numOfData))
 pickItemList = randperl(numOfData)
 jpnTrainDoc = copy(doc[pickItemList[1:numOfTrainData]])
 jpnTestDoc = copy(doc[pickItemList[(numOfTrainData+1):numOfData]])
