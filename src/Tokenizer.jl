9fd4d15e7dfbeac8a40a06e901654d648729623b
tomeken-yoshinaga <kenta-yoshinaga@aist.go.jp>
2016-12-12 16:37:38 +0900

    [update] Modify model to satisfy the latest Merlin

--- src/Tokenizer.jl
+++ src/Tokenizer.jl
@@ -16,25 +16,40 @@ function Tokenizer(prefix::String = "";emboutCh=32,convFilterWidth=3)
   convPadWidth = Int((convFilterWidth - 1)/2)
   lsOutCh = 64
   lsOutCh2 = 4
-  embed = Embedding(T, 1000000, emboutCh)
-  conv = Conv(T, (emboutCh,convFilterWidth),(1,convOutCh),paddims=(0,convPadWidth))
-  ls = Linear(T, convOutCh, lsOutCh)
-  ls2 = Linear(T, lsOutCh, lsOutCh2)
-  g = @graph begin
-    chars = identity(:chars)
-    x = Var(reshape(chars, 1, length(chars)))
-    x = embed(x)
+
+#  embed = Embedding(T, 1000000, emboutCh)
+#  conv = Conv(T, (emboutCh,convFilterWidth),(1,convOutCh),paddims=(0,convPadWidth))
+#  ls = Linear(T, convOutCh, lsOutCh)
+#  ls2 = Linear(T, lsOutCh, lsOutCh2)
+
+  x = Var()
+  y = Embedding(T, 100000, emboutCh)(x)
+  y = Conv(T, (emboutCh, convFilterWidth), (1,convOutCh), paddims=(0,convOutCh))(y)
+  y = reshape(y, size(y,2), size(y,3))
+  y = transpose(y)
+  y = relu(y)
+  y = dropout(y, 0.5, true)(y)
+  y = Linear(T, convOutCh, lsOutCh)(y)
+  y = dropout(y, 0.25, true)(y)
+  y = Linear(T, lsOutCh, lsOutCh2)
+  g = compile(y, x)
+
+#  g = @graph begin
+#    chars = identity(:chars)
+#    x = Var(reshape(chars, 1, length(chars)))
+#    x = embed(x)
+##	x = dropout(x, 0.25, true)
+#    x = conv(x)
+#    x = reshape(x, size(x, 2), size(x, 3))
+#    x = transpose(x)
+#    x = relu(x)
+#	x = dropout(x, 0.5, true)
+#    x = ls(x)
 #	x = dropout(x, 0.25, true)
-    x = conv(x)
-    x = reshape(x, size(x, 2), size(x, 3))
-    x = transpose(x)
-    x = relu(x)
-	x = dropout(x, 0.5, true)
-    x = ls(x)
-	x = dropout(x, 0.25, true)
-    x = ls2(x)
-    x
-  end
+#    x = ls2(x)
+#    x
+#  end
+
   if length(prefix) > 0
     outf = open(string("./data/",prefix,"/NetworkConstruction.tsv"),"w")
     write(outf, "Embediding: ($(length(embed.ws)),$(length(embed.ws[1].data)))\n")
